c6c64ef06408add4b0aac8f363509036d85d315e
长意 <65816050+changyi1130@users.noreply.github.com>
2024-04-24 16:48:26 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // 绑定点击事件到所有类名为 'copy_cell' 的单元格
+    document.querySelectorAll('.copy_cell').forEach(cell => {
+        cell.addEventListener('click', function () {
+            // 使用 Clipboard API 来写入剪贴板
+            navigator.clipboard.writeText(this.innerText).then(() => {
+                // 复制成功的回调
+                console.log(this.innerText)
+            }).catch(err => {
+                // 复制失败的回调
+                console.error('复制失败:', err);
+            });
+        });
+    });
+});
